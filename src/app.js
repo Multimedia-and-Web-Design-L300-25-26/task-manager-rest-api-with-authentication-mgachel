b79a80adc2b0807c54c259a1d25a71770d1ad40c
mgachel <mgachel@st.ug.edu.gh>
2026-03-05 01:03:26 +0000

    updated fix blah

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
+
+// Connect to MongoDB (top-level await ensures connection before app is used)
+await connectDB();
 
 const app = express();
 
